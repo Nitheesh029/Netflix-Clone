5e3811cf9d03ae5df41eb11e3d649f712c133866
Nitheesh <qxnitheesh@gmail.com>
2025-02-02 06:33:38 +0530

    make the movie card section responsive

--- src/script.js
+++ src/script.js
@@ -33,7 +33,7 @@ const generateCards = (datas) => {
     html += `
           <div class="w-[112px] md:w-[200px] relative mt-10 cursor-pointer hover:-translate-y-10 duration-300 shadow-slate-900 shadow-xl">
               <img src="/images/movie${data.number}.jpg" alt="" class="rounded-xl object-cover w-full">
-              <div class="absolute text-black bottom-0 font-bold text-6xl md:text-8xl custom-outline">${data.number}</div>
+              <div class="absolute text-black left-[-15px] bottom-0 font-bold text-6xl md:text-8xl custom-outline">${data.number}</div>
           </div>
       `;
   });
